a1abfc8843357adc93e4a83a357dd3b5a84f597f
Gabriel Bernardi <>
2016-11-09 00:40:19 -0200

    Criado o Frame de teste para executar a intrução

--- MVM/src/br/furb/nucleo/MVM.java
+++ MVM/src/br/furb/nucleo/MVM.java
@@ -1,9 +1,7 @@
 package br.furb.nucleo;
 
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
